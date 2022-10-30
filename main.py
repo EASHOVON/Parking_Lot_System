3576753b09c85ed29578bd67bf87a027eb7b46c4
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-30 13:15:26 +0600

    Added a method of Class Garage add_car_to_garage

--- main.py
+++ main.py
@@ -15,5 +15,21 @@ class Garage:
     def spot_available(self):
         return f"Total Spots Available {self.spot}"
 
-    
+    def add_car_to_garage(self,car):
+        self.spot_name = ['A1','B1','C1','D1','E1','F1','G1','H1','I1','J1']
+        if self.spot > 0:
+            user_data = str(car).split(',')
+            self.spot -= 1
+            self.car_added.append(user_data)
+            self.car_infos = {'Tickets': [],'License':[],'Model':[],'Color':[]}
+            ticket = ''
+            for i, val in enumerate(self.car_added):
+                ticket = self.spot_name[i] + val[0]
+                self.car_infos['Tickets'].append(ticket)
+                self.car_infos['License'].append(val[0])
+                self.car_infos['Model'].append(val[1])
+                self.car_infos['Color'].append(val[2])
+            print(f"Successfully Parked!!! YOUR TICKET {ticket}")
+        else:
+            print("NO SPOTS AVAILABLE!!!!!!")
 
