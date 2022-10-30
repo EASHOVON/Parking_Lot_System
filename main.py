82f6c4a115cf47f606da22080863d8148b639924
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-30 12:51:18 +0600

    Added Garage Class. Initialize it and added a method spot_available

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ class Car:
         self.license = license
         self.model = model
         self.color = color
+    def __repr__(self) -> str:
+        return f"{self.license}, {self.model}, {self.color}"
+
+class Garage:
+    def __init__(self) -> None:
+        self.car_added = []
+        self.spot = 10
+        self.car_infos = {}
+
+    def spot_available(self):
+        return f"Total Spots Available {self.spot}"
+
+    
+
